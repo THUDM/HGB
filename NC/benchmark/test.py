8d6b08234a785a5554c5308b93acfcb7d22ffc9f
Your Name <you@example.com>
2020-12-22 17:19:24 +0800

    update get idx example

--- NC/benchmark/test.py
+++ NC/benchmark/test.py
@@ -7,3 +7,10 @@ print(dl.labels_train)
 print(dl.labels_train['data'][dl.labels_train['mask']])
 pred = dl.labels_test['data'][dl.labels_test['mask']]
 print(dl.evaluate(pred))
+
+train_idx = np.nonzero(dl.labels_train['mask'])[0]
+test_idx = np.nonzero(dl.labels_test['mask'])[0]
+print(train_idx)
+print(train_idx.shape)
+print(test_idx)
+print(test_idx.shape)
