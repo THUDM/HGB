f6f7638e7fff149def5cbab19270264e2f291372
Your Name <you@example.com>
2021-01-15 10:47:32 +0800

    first version of new baseline LP

---
+++ LP/benchmark/methods/baseline/GNN.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+import dgl
+from dgl.nn.pytorch import GraphConv
+
+import dgl.function as fn
+from dgl.nn.pytorch import edge_softmax, GATConv
+from conv import myGATConv
+
+class DistMult(nn.Module):
+    def __init__(self, num_rel, dim):
+        super(DistMult, self).__init__()
+        self.W = nn.Parameter(torch.FloatTensor(size=(num_rel, dim, dim)))
+        nn.init.xavier_normal_(self.W, gain=1.414)
+
+    def forward(self, left_emb, right_emb, r_id):
+        thW = self.W[r_id]
+        left_emb = torch.unsqueeze(left_emb, 1)
+        right_emb = torch.unsqueeze(right_emb, 2)
+        return torch.bmm(torch.bmm(left_emb, thW), right_emb).squeeze()
+
+class Dot(nn.Module):
+    def __init__(self):
+        super(Dot, self).__init__()
+    def forward(self, left_emb, right_emb, r_id):
+        left_emb = torch.unsqueeze(left_emb, 1)
+        right_emb = torch.unsqueeze(right_emb, 2)
+        return torch.bmm(left_emb, right_emb).squeeze()
+
+class myGAT(nn.Module):
+    def __init__(self,
+                 g,
+                 edge_dim,
+                 num_etypes,
+                 in_dims,
+                 num_hidden,
+                 num_classes,
+                 num_layers,
+                 heads,
+                 activation,
+                 feat_drop,
+                 attn_drop,
+                 negative_slope,
+                 residual,
+                 alpha,
+                 decode='distmult'):
+        super(myGAT, self).__init__()
+        self.g = g
+        self.num_layers = num_layers
+        self.gat_layers = nn.ModuleList()
+        self.activation = activation
+        self.fc_list = nn.ModuleList([nn.Linear(in_dim, num_hidden, bias=True) for in_dim in in_dims])
+        for fc in self.fc_list:
+            nn.init.xavier_normal_(fc.weight, gain=1.414)
+        # input projection (no residual)
+        self.gat_layers.append(myGATConv(edge_dim, num_etypes,
+            num_hidden, num_hidden, heads[0],
+            feat_drop, attn_drop, negative_slope, False, self.activation, alpha=alpha))
+        # hidden layers
+        for l in range(1, num_layers):
+            # due to multi-head, the in_dim = num_hidden * num_heads
+            self.gat_layers.append(myGATConv(edge_dim, num_etypes,
+                num_hidden * heads[l-1], num_hidden, heads[l],
+                feat_drop, attn_drop, negative_slope, residual, self.activation, alpha=alpha))
+        # output projection
+        self.gat_layers.append(myGATConv(edge_dim, num_etypes,
+            num_hidden * heads[-2], num_classes, heads[-1],
+            feat_drop, attn_drop, negative_slope, residual, None, alpha=alpha))
+        self.epsilon = torch.FloatTensor([1e-12]).cuda()
+        if decode == 'distmult':
+            self.decoder = DistMult(num_etypes, num_classes)
+        elif decode == 'dot':
+            self.decoder = Dot()
+
+    def forward(self, features_list, e_feat, left, right, mid):
+        h = []
+        for fc, feature in zip(self.fc_list, features_list):
+            h.append(fc(feature))
+        h = torch.cat(h, 0)
+        res_attn = None
+        for l in range(self.num_layers):
+            h, res_attn = self.gat_layers[l](self.g, h, e_feat, res_attn=res_attn)
+            h = h.flatten(1)
+        # output projection
+        logits, _ = self.gat_layers[-1](self.g, h, e_feat, res_attn=None)
+        logits = logits.mean(1)
+        # This is an equivalent replacement for tf.l2_normalize, see https://www.tensorflow.org/versions/r1.15/api_docs/python/tf/math/l2_normalize for more information.
+        logits = logits / (torch.max(torch.norm(logits, dim=1, keepdim=True), self.epsilon))
+        left_emb = logits[left]
+        right_emb = logits[right]
+        return self.decoder(left_emb, right_emb, mid)
+
---
+++ LP/benchmark/methods/baseline/README.md
@@ -0,0 +1,23 @@
+# new baseline for benchmark
+
+For message passing with relation attention version:
+
+```
+python run_new.py --dataset DBLP
+python run_new.py --dataset ACM --feats-type 2
+python run_multi.py --dataset IMDB --feats-type 0
+```
+
+For relational gat version:
+
+```
+python run.py --dataset DBLP
+```
+
+## running environment
+
+* torch 1.6.0 cuda 10.1
+* dgl 0.4.3 cuda 10.1
+* networkx 2.3
+* scikit-learn 0.23.2
+* scipy 1.5.2
