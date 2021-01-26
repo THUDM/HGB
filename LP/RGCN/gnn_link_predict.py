992f12ce692428a2a63edda3319c198b88bf6557
cyx233 <chenyuxi18@mails.tsinghua.edu.cn>
2021-01-26 23:27:10 +0800

    RGCN LP

---
+++ LP/RGCN/gnn_link_predict.py
@@ -0,0 +1,226 @@
+"""
+Modeling Relational Data with Graph Convolutional Networks
+Paper: https://arxiv.org/abs/1703.06103
+Code: https://github.com/MichSchli/RelationPrediction
+Difference compared to MichSchli/RelationPrediction
+* Report raw metrics instead of filtered metrics.
+* By default, we use uniform edge sampling instead of neighbor-based edge
+  sampling used in author's code. In practice, we find it achieves similar MRR
+  probably because the model only uses one GNN layer so messages are propagated
+  among immediate neighbors. User could specify "--edge-sampler=neighbor" to switch
+  to neighbor-based edge sampling.
+"""
+
+import argparse
+import numpy as np
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+from dgl.data.knowledge_graph import load_data
+from dgl.nn.pytorch import RelGraphConv
+import dgl
+
+from model import GCN, GAT
+
+import utils
+
+
+def main(args):
+    # load graph data
+    data = load_data(args.dataset)
+    num_nodes = data.num_nodes
+    train_data = data.train
+    valid_data = data.valid
+    test_data = data.test
+    num_rels = data.num_rels
+
+    # check cuda
+    use_cuda = args.gpu >= 0 and torch.cuda.is_available()
+    if use_cuda:
+        torch.cuda.set_device(args.gpu)
+
+    # create model
+    if args.model == 'gcn':
+        model = GCN(in_feats=[num_nodes], hid_feats=args.n_hidden,
+                    n_layers=args.n_layers, dropout=args.dropout, rel_num=num_rels)
+    elif args.model == 'gat':
+        heads = [args.n_heads] * args.n_layers + [1]
+        model = GAT(in_feats=[num_nodes], hid_feats=args.n_hidden,
+                    n_layers=args.n_layers, heads=heads, dropout=args.dropout, rel_num=num_rels)
+    else:
+        exit(0)
+    # validation and testing triplets
+    valid_data = torch.LongTensor(valid_data)
+    test_data = torch.LongTensor(test_data)
+
+    # build test graph
+    test_graph, test_rel, test_norm = utils.build_test_graph(
+        num_nodes, num_rels, train_data)
+    test_graph = dgl.add_self_loop(test_graph)
+
+    indices = np.vstack((np.arange(num_nodes), np.arange(num_nodes)))
+    indices = torch.LongTensor(indices)
+    values = torch.FloatTensor(np.ones(num_nodes))
+    test_feat = torch.sparse.FloatTensor(
+        indices, values, torch.Size([num_nodes, num_nodes]))
+    if use_cuda:
+        test_feat = test_feat.cuda()
+
+    if use_cuda:
+        model.cuda()
+
+    # build adj list and calculate degrees for sampling
+    adj_list, degrees = utils.get_adj_and_degrees(num_nodes, train_data)
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    model_state_file = args.model + 'model_state.pth'
+    forward_time = []
+    backward_time = []
+
+    # training loop
+    print("start training...")
+
+    epoch = 0
+    best_mrr = 0
+    while True:
+        model.train()
+        epoch += 1
+
+        # perform edge neighborhood sampling to generate training graph and data
+        g, node_id, edge_type, node_norm, data, labels = \
+            utils.generate_sampled_graph_and_labels(train_data, args.graph_batch_size, args.graph_split_size,
+                                                    num_rels, adj_list, degrees, args.negative_sample,
+                                                    args.edge_sampler)
+        print("Done edge sampling")
+        if not model == "rgcn":
+            g = dgl.add_self_loop(g)
+        sample_num = g.num_nodes()
+
+        # set node/edge feature
+        node_id = torch.from_numpy(node_id).view(-1, 1).long()
+        edge_type = torch.from_numpy(edge_type)
+        data, labels = torch.from_numpy(data), torch.from_numpy(labels)
+        deg = g.in_degrees(range(g.number_of_nodes())).float().view(-1, 1)
+        if use_cuda:
+            node_id, deg = node_id.cuda(), deg.cuda()
+            edge_type = edge_type.cuda()
+            data, labels = data.cuda(), labels.cuda()
+            g = g.to(args.gpu)
+
+        indices = np.vstack((np.arange(sample_num), np.arange(sample_num)))
+        indices = torch.LongTensor(indices)
+        values = torch.FloatTensor(np.ones(sample_num))
+        feat = torch.sparse.FloatTensor(
+            indices, values, torch.Size([sample_num, num_nodes]))
+        if use_cuda:
+            feat = feat.cuda()
+
+        t0 = time.time()
+        hid_feat = model.encode([g, [feat]])
+        r_list = data[:, 1]
+        out_feat = model.decode(
+            r_list, hid_feat[data[:, 0]], hid_feat[data[:, 2]])
+        out_feat = torch.sigmoid(out_feat)
+        loss = F.binary_cross_entropy_with_logits(out_feat, labels)
+        t1 = time.time()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(
+            model.parameters(), args.grad_norm)  # clip gradients
+        optimizer.step()
+        t2 = time.time()
+
+        forward_time.append(t1 - t0)
+        backward_time.append(t2 - t1)
+        print("Epoch {:04d} | Loss {:.10f} | Best MRR {:.4f} | Forward {:.4f}s | Backward {:.4f}s".
+              format(epoch, loss.item(), best_mrr, forward_time[-1], backward_time[-1]))
+
+        optimizer.zero_grad()
+
+        # validation
+        if epoch % args.evaluate_every == 0:
+            # perform validation on CPU because full graph is too large
+            model.eval()
+            if use_cuda:
+                model.cpu()  # test on CPU
+                test_feat = test_feat.cpu()
+            print("start eval")
+            embed = model.encode([test_graph, [test_feat]])
+            mrr = utils.calc_mrr(embed, model.decode.weights, torch.LongTensor(train_data),
+                                 valid_data, test_data, hits=[1, 3, 10], eval_bz=args.eval_batch_size,
+                                 eval_p=args.eval_protocol)
+            # save best model
+            if mrr < best_mrr:
+                if epoch >= args.n_epochs:
+                    break
+            else:
+                best_mrr = mrr
+                torch.save({'state_dict': model.state_dict(), 'epoch': epoch},
+                           model_state_file)
+            if use_cuda:
+                model.cuda()
+
+    print("training done")
+    print("Mean forward time: {:4f}s".format(np.mean(forward_time)))
+    print("Mean Backward time: {:4f}s".format(np.mean(backward_time)))
+
+    print("\nstart testing:")
+    # use best model checkpoint
+    checkpoint = torch.load(model_state_file)
+    if use_cuda:
+        model.cpu()  # test on CPU
+        test_feat = test_feat.cpu()
+    model.eval()
+    model.load_state_dict(checkpoint['state_dict'])
+    print("Using best epoch: {}".format(checkpoint['epoch']))
+    embed = model.encode([test_graph, [test_feat]])
+    utils.calc_mrr(embed, model.decode.weights, torch.LongTensor(train_data), valid_data,
+                   test_data, hits=[1, 3, 10], eval_bz=args.eval_batch_size, eval_p=args.eval_protocol)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RGCN')
+    parser.add_argument("--dropout", type=float, default=0.2,
+                        help="dropout probability")
+    parser.add_argument("--n-hidden", type=int, default=500,
+                        help="number of hidden units")
+    parser.add_argument("--gpu", type=int, default=-1,
+                        help="gpu")
+    parser.add_argument("--lr", type=float, default=1e-2,
+                        help="learning rate")
+    parser.add_argument("--n-bases", type=int, default=100,
+                        help="number of weight blocks for each relation")
+    parser.add_argument("--n-layers", type=int, default=2,
+                        help="number of propagation rounds")
+    parser.add_argument("--n-epochs", type=int, default=6000,
+                        help="number of minimum training epochs")
+    parser.add_argument("-d", "--dataset", type=str, required=True,
+                        help="dataset to use")
+    parser.add_argument("--eval-batch-size", type=int, default=500,
+                        help="batch size when evaluating")
+    parser.add_argument("--eval-protocol", type=str, default="filtered",
+                        help="type of evaluation protocol: 'raw' or 'filtered' mrr")
+    parser.add_argument("--regularization", type=float, default=0.01,
+                        help="regularization weight")
+    parser.add_argument("--grad-norm", type=float, default=1.0,
+                        help="norm to clip gradient to")
+    parser.add_argument("--graph-batch-size", type=int, default=30000,
+                        help="number of edges to sample in each iteration")
+    parser.add_argument("--graph-split-size", type=float, default=0.5,
+                        help="portion of edges used as positive sample")
+    parser.add_argument("--negative-sample", type=int, default=10,
+                        help="number of negative samples per positive sample")
+    parser.add_argument("--evaluate-every", type=int, default=500,
+                        help="perform evaluation every n epochs")
+    parser.add_argument("--n-heads", type=int, default=4,
+                        help="heads for gat")
+    parser.add_argument("--edge-sampler", type=str, default="uniform",
+                        help="type of edge sampler: 'uniform' or 'neighbor'")
+    parser.add_argument("--model", type=str, default="gcn",
+                        help="type of model")
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
---
+++ LP/RGCN/model.py
@@ -0,0 +1,227 @@
+import torch as th
+from torch._C import Graph
+import torch.nn as nn
+import torch as th
+from torch import nn
+import torch.nn.functional as F
+from dgl.nn.pytorch import GATConv, GraphConv
+from torch.nn.modules import activation
+
+
+class DisMult(th.nn.Module):
+    def __init__(self, rel_num, dim):
+        super(DisMult, self).__init__()
+        self.dim = dim
+        self.weights = nn.Parameter(th.FloatTensor(size=(rel_num, dim)))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_normal_(self.weights, gain=1.414)
+
+    def forward(self, r_list, input1, input2):
+        r = self.weights[r_list]
+        return th.sum(input1 * r * input2, dim=1)
+
+
+class GCN(th.nn.Module):
+    def __init__(self, in_feats, hid_feats, n_layers=2, dropout=0.5,  rel_num=1):
+        super(GCN, self).__init__()
+        self.fc_list = nn.ModuleList(
+            [nn.Linear(feats_dim, hid_feats, bias=True) for feats_dim in in_feats])
+        for fc in self.fc_list:
+            nn.init.xavier_normal_(fc.weight, gain=1.414)
+        self.layers = nn.ModuleList()
+        # input layer
+        self.layers.append(GraphConv(hid_feats, hid_feats))
+        # hidden layers
+        for i in range(n_layers - 1):
+            self.layers.append(GraphConv(hid_feats, hid_feats))
+        self.dropout = nn.Dropout(p=dropout)
+        for layer in self.layers:
+            nn.init.xavier_normal_(layer.weight, gain=1.4)
+        self.decode = DisMult(rel_num=rel_num, dim=hid_feats)
+
+    def encode(self, data):
+        g, feats_list = data
+        h = []
+        for fc, feature in zip(self.fc_list, feats_list):
+            h.append(fc(feature))
+        x = th.cat(h, 0)
+        for i, layer in enumerate(self.layers):
+            if i != 0:
+                x = self.dropout(x)
+            x = layer(g, x)
+            if i < len(self.layers) - 1:
+                x = F.elu(x)
+        return x
+
+
+class GAT(th.nn.Module):
+    def __init__(self, in_feats, hid_feats, n_layers=2, dropout=0.5, heads=[1], rel_num=1):
+        super(GAT, self).__init__()
+        self.fc_list = nn.ModuleList(
+            [nn.Linear(feats_dim, hid_feats, bias=True) for feats_dim in in_feats])
+        for fc in self.fc_list:
+            nn.init.xavier_normal_(fc.weight, gain=1.414)
+        self.layers = nn.ModuleList()
+        # input layer
+        self.layers.append(GATConv(hid_feats, hid_feats,
+                                   heads[0], activation=F.elu))
+        # hidden layers
+        for l in range(1, n_layers - 1):
+            self.layers.append(
+                GATConv(hid_feats * heads[l - 1], hid_feats, heads[l], activation=F.elu))
+        # output layer
+        self.layers.append(
+            GATConv(hid_feats * heads[-2], hid_feats, heads[-1], activation=None))
+        self.dropout = nn.Dropout(p=dropout)
+        for layer in self.layers:
+            layer.reset_parameters()
+        self.decode = DisMult(rel_num=rel_num, dim=hid_feats)
+
+    def encode(self, data):
+        g, feats_list = data
+        h = []
+        for fc, feature in zip(self.fc_list, feats_list):
+            h.append(fc(feature))
+        x = th.cat(h, 0)
+        for i, layer in enumerate(self.layers):
+            if i != 0:
+                x = self.dropout(x)
+            x = layer(g, x).flatten(1)
+        return x
+
+
+class BaseRGCN(nn.Module):
+    def __init__(self, num_nodes, h_dim, out_dim, num_rels, num_bases,
+                 num_hidden_layers=1, dropout=0,
+                 use_self_loop=False, use_cuda=False):
+        super(BaseRGCN, self).__init__()
+        self.num_nodes = num_nodes
+        self.h_dim = h_dim
+        self.out_dim = out_dim
+        self.num_rels = num_rels
+        self.num_bases = None if num_bases < 0 else num_bases
+        self.num_hidden_layers = num_hidden_layers
+        self.dropout = dropout
+        self.use_self_loop = use_self_loop
+        self.use_cuda = use_cuda
+
+        # create rgcn layers
+        self.build_model()
+
+    def build_model(self):
+        self.layers = nn.ModuleList()
+        # i2h
+        i2h = self.build_input_layer()
+        if i2h is not None:
+            self.layers.append(i2h)
+        # h2h
+        for idx in range(self.num_hidden_layers):
+            h2h = self.build_hidden_layer(idx)
+            self.layers.append(h2h)
+        # h2o
+        h2o = self.build_output_layer()
+        if h2o is not None:
+            self.layers.append(h2o)
+
+    def build_input_layer(self):
+        return None
+
+    def build_hidden_layer(self, idx):
+        raise NotImplementedError
+
+    def build_output_layer(self):
+        return None
+
+    def forward(self, g, h, r, norm):
+        for layer in self.layers:
+            h = layer(g, h, r, norm)
+        return h
+
+
+class RelGraphEmbedLayer(nn.Module):
+    r"""Embedding layer for featureless heterograph.
+    Parameters
+    ----------
+    dev_id : int
+        Device to run the layer.
+    num_nodes : int
+        Number of nodes.
+    node_tides : tensor
+        Storing the node type id for each node starting from 0
+    num_of_ntype : int
+        Number of node types
+    input_size : list of int
+        A list of input feature size for each node type. If None, we then
+        treat certain input feature as an one-hot encoding feature.
+    embed_size : int
+        Output embed size
+    embed_name : str, optional
+        Embed name
+    """
+
+    def __init__(self,
+                 dev_id,
+                 num_nodes,
+                 node_tids,
+                 num_of_ntype,
+                 input_size,
+                 embed_size,
+                 sparse_emb=False,
+                 embed_name='embed'):
+        super(RelGraphEmbedLayer, self).__init__()
+        self.dev_id = th.device(dev_id if dev_id >= 0 else 'cpu')
+        self.embed_size = embed_size
+        self.embed_name = embed_name
+        self.num_nodes = num_nodes
+        self.sparse_emb = sparse_emb
+
+        # create weight embeddings for each node for each relation
+        self.embeds = nn.ParameterDict()
+        self.num_of_ntype = num_of_ntype
+        self.idmap = th.empty(num_nodes).long()
+
+        for ntype in range(num_of_ntype):
+            if input_size[ntype] is not None:
+                input_emb_size = input_size[ntype].shape[1]
+                embed = nn.Parameter(
+                    th.Tensor(input_emb_size, self.embed_size))
+                nn.init.xavier_uniform_(embed)
+                self.embeds[str(ntype)] = embed
+
+        self.node_embeds = th.nn.Embedding(
+            node_tids.shape[0], self.embed_size, sparse=self.sparse_emb)
+        nn.init.uniform_(self.node_embeds.weight, -1.0, 1.0)
+
+    def forward(self, node_ids, node_tids, type_ids, features):
+        """Forward computation
+        Parameters
+        ----------
+        node_ids : tensor
+            node ids to generate embedding for.
+        node_ids : tensor
+            node type ids
+        features : list of features
+            list of initial features for nodes belong to different node type.
+            If None, the corresponding features is an one-hot encoding feature,
+            else use the features directly as input feature and matmul a
+            projection matrix.
+        Returns
+        -------
+        tensor
+            embeddings as the input of the next layer
+        """
+        tsd_ids = node_ids.to(self.node_embeds.weight.device)
+        embeds = th.empty(node_ids.shape[0],
+                          self.embed_size, device=self.dev_id)
+        for ntype in range(self.num_of_ntype):
+            if features[ntype] is not None:
+                loc = node_tids == ntype
+                embeds[loc] = features[ntype][type_ids[loc]].to(
+                    self.dev_id) @ self.embeds[str(ntype)].to(self.dev_id)
+            else:
+                loc = node_tids == ntype
+                embeds[loc] = self.node_embeds(tsd_ids[loc]).to(self.dev_id)
+
+        return embeds
